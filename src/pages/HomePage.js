bf8cb7580e6afb2a52aa17351fb2c96a25deeb4f
SoumikGhosh111 <soumikbuie2001@gmail.com>
2024-03-27 13:06:42 +0530

    our process is hidden

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import HeroSection from '../components/HeroSection/HeroSection'; 
 import MarqueeImages from '../common-components/Marquee/MarqueeImages';
 import AboutUsComp from '../components/About/AboutUsComp';
@@ -9,18 +9,20 @@ import OurValues from '../components/OurValues/OurValues';
 import TempComponent from '../common-components/TempComponenet/TempComponent';
 import Blogs from '../components/Blogs/Blogs';
 function HomePage() {
+  // const [speed, setSpeed] = useState(0);
+  // const [lastUpdateTime, setLastUpdateTime] = useState(0);
   return (
-    <>
+    <div className='homePage'>
         <HeroSection />
         <MarqueeImages />
         <AboutUsComp />
         <Services />
-        <OurProcess />
+        {/* <OurProcess /> */}
         <OurMisson />
         <TempComponent />
         <OurValues />
         <Blogs />
-    </>
+    </div>
   )
 }
 
